cc5078c0d2965d89bbe2d2df31971fa6db0fbe35
Jeroen Roovers <jroovers93@gmail.com>
2022-04-12 17:19:56 +0200

    Fix build

---
+++ src/test/java/io/github/jroovers/kafkaesque/consumer/KafkaesqueConsumerTest.java
@@ -0,0 +1,11 @@
+package io.github.jroovers.kafkaesque.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaesqueConsumerTest {
+    @Test
+    void contextLoads() {
+    }
+}
--- src/test/java/io/github/jroovers/kafkaesque/producer/KafkaesqueProducerTest.java
+++ src/test/java/io/github/jroovers/kafkaesque/producer/KafkaesqueProducerTest.java
@@ -1,13 +1,11 @@
-package io.github.jroovers.kafkaesque;
+package io.github.jroovers.kafkaesque.producer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class KafkaesqueApplicationTests {
-
+class KafkaesqueProducerTest {
     @Test
     void contextLoads() {
     }
-
 }
